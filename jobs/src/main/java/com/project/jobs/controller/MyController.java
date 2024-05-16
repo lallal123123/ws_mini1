424bac2e291241fecf77844abfe155229a12390b
junhyeong <wnsgud0202@naver.com>
2024-05-16 20:09:46 +0900

    매퍼 초기 설정 완료

--- jobs/src/main/java/com/project/jobs/controller/MyController.java
+++ jobs/src/main/java/com/project/jobs/controller/MyController.java
@@ -1,14 +1,20 @@
 package com.project.jobs.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.project.jobs.dao.IMemberDao;
 
 @Controller
 public class MyController {
-
+	@Autowired
+	IMemberDao dao;
+	
 	@RequestMapping("/")
 	public  String root(){
+		dao.reg();
+		
 		return "index";
 	}
 }
